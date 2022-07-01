2f4498bff94fb03ef3eacdd89ed176b4a9695727
Rafa Kuchta <rafal.kuchta1@gmail.com>
2022-07-01 10:06:53 +0200

    Edited cors config

--- index.ts
+++ index.ts
@@ -4,11 +4,12 @@ import 'express-async-errors';
 import { handleError } from './utils/errors';
 import rateLimit from 'express-rate-limit';
 import {todoRouter} from "./routers/todo.router";
+import { config } from './config/config';
 
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: config.corsOrigin,
 }));
 app.use(json());
 // app.use(rateLimit({
